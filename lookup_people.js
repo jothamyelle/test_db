4f0ed37daf9f6f5b4ea004ae932e711e1b9aa1af
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-21 02:46:41 +0000

    Implemented lookup functionality

--- lookup_people.js
+++ lookup_people.js
@@ -1,5 +1,8 @@
+console.log('Searching...');
+
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
+const personToLookup = process.argv.slice(2);
 
 const client = new pg.Client({
   user     : settings.user,
@@ -14,12 +17,21 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+  client.query(`SELECT DISTINCT first_name AS person_found,  last_name, birthdate
+                FROM famous_people 
+                WHERE first_name = '${personToLookup}'`, (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log('result:', result);
-    console.log(result.rows[0].number); //output: 1
+    let numOfResults = result.rowCount;
+    
+    console.log(`Found ${numOfResults} person(s) by the name '${personToLookup}'`);
+    for (let i = 0; i < numOfResults; i++) {
+      let name = result.rows[i].person_found;
+      let lastName = result.rows[i].last_name;
+      let birthdate = result.rows[i].birthdate.toISOString().split('T')[0];
+      console.log(`- ${i + 1}: ${name} ${lastName}, born '${birthdate}'`);
+    }
     client.end();
   });
 });
